eab6214fc0cbfbc5167087f97a3573c1c2e1b6e0
carldomond7 <124755395+carldomond7@users.noreply.github.com>
2024-03-09 11:14:39 -0800

    Update lpu.py

--- lpu.py
+++ lpu.py
@@ -4,7 +4,6 @@ import uvicorn
 import os
 from langchain.chains import LLMChain  # Update with the correct import based on your langchain package
 from langchain.prompts import PromptTemplate  # Update with the correct import based on your langchain package
-#rom langchain_core.prompts import ChatPromptTemplate  # Uncomment if actually used
 from langchain_groq import ChatGroq  # Update with the correct import based on your langchain package
 
 class UserRequest(BaseModel):
@@ -13,35 +12,40 @@ class UserRequest(BaseModel):
 
 app = FastAPI()
 
-llm = ChatGroq(groq_api_key="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", model_name='mixtral-8x7b-32768')
+@app.get("/")
+async def root():
+    return {"message": "plswork!"}
+
 
 @app.post("/route/")
-async def process_request(user_request: UserRequest): 
+async def process_request(user_request: UserRequest):
+    llm = ChatGroq(groq_api_key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", model_name='mixtral-8x7b-32768')
+
     query = user_request.query
     content = user_request.content
 
     prompt_template = """ You are a professional recruiter who specializes in cultivating talent, you are very knowledgable about all types of jobs. Answer the question
     based on the context below.
-    
+
     Context: {content}
-    
+
 
     Question: {query}
     """
 
 # Define the prompt structure
     prompt = PromptTemplate(
-    input_variables=["content", "query"],  
+    input_variables=["content", "query"],
     template=prompt_template,
 )
 
 
     llm_chain = LLMChain(llm=llm, prompt=prompt)
-    
+
     # Pass the context and question to the Langchain chain
     result_chain = llm_chain.invoke({"content": content, "query": query})
 
     return result_chain
 
-    if __name__ == "__main__":
-    uvicorn.run(app)
+if __name__ == "__main__":
+        uvicorn.run(app)
